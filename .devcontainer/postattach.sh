8d0d192308f01a72c4e8861572dbb9e7f587bdd8
Soojin (Min) Choi <minsa110@gmail.com>
2023-04-03 11:02:59 -0700

    add postattach file

---
+++ .devcontainer/postattach.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+poetry env use 'python3.10'
+poetry install --no-root
+export DATASTORE=redis
+export BEARER_TOKEN=footoken
+export OPENAI_API_KEY='source .env'
+docker compose -f ./examples/docker/redis/docker-compose.yml up -d
