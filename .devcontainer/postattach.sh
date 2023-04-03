39f2933c0a88c777ccae227297f031249eda2b2e
Soojin (Min) Choi <minsa110@gmail.com>
2023-04-03 16:38:39 -0700

    env file creation

--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -2,7 +2,10 @@
 set -e
 poetry env use 'python3.10'
 poetry install --no-root
-export DATASTORE=redis
-export BEARER_TOKEN=footoken
-export OPENAI_API_KEY='source .env'
+cat > .env << EOF
+EXPORT DATASTORE=redis
+EXPORT BEARER_TOKEN=footoken
+EXPORT OPENAI_API_KEY = ''
+EOF
+source .env
 docker compose -f ./examples/docker/redis/docker-compose.yml up -d
