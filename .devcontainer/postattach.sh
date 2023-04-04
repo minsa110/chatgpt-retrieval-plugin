996259bee97e484467e08f51353379e49fe66415
Soojin (Min) Choi <minsa110@gmail.com>
2023-04-04 09:49:18 -0700

    sevenv

--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-set -e
-poetry env use 'python3.10'
-poetry install --no-root
-source .env
+set -e &&\
+poetry env use 'python3.10' &&\
+poetry install --no-root &&\
+sudo chmod +x ./.devcontainer/setenv.sh &&\
+./.devcontainer/setenv.sh &&\
+source .env &&\
 docker compose -f ./examples/docker/redis/docker-compose.yml up -d
