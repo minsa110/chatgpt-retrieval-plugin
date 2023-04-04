aac66c64239a99bc6835b98a91248152937b73ce
Soojin (Min) Choi <minsa110@gmail.com>
2023-04-04 00:17:00 +0000

    wait using and

--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -2,11 +2,10 @@
 set -e
 poetry env use 'python3.10'
 poetry install --no-root
-cat > .env << EOF
+cat > .env << EOF &&
 export DATASTORE=redis
 export BEARER_TOKEN=footoken
 export OPENAI_API_KEY=''
 EOF
-wait
-source .env
+source .env &&
 docker compose -f ./examples/docker/redis/docker-compose.yml up -d
